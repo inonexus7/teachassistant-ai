a9f00595eb716e2e419d026e1930b8bf9ff7e10a
inonexus <inonexus7@gmail.com>
2025-02-27 20:31:04 +0200

    updated

--- Backend/ytgpt.py
+++ Backend/ytgpt.py
@@ -3,16 +3,10 @@ from openai import OpenAI
 import config
 import json
 import re
-import os
 import textwrap
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
 from gptutils import create_chat_data
-import moviepy.editor as mp 
-import speech_recognition as sr
-import datetime
-import requests
-from pytube import YouTube
 import config
 
 openai_api_key = config.DevelopmentConfig.OPENAI_KEY
@@ -31,22 +25,21 @@ def savehistory(filename, messages):
         json.dump(messages, outfile)
 
 def aicomplete(prompt, filename):
-    openai.api_key = config.DevelopmentConfig.OPENAI_KEY
-    completion = openai.ChatCompletion()
+    # openai.api_key = config.DevelopmentConfig.OPENAI_KEY
     model = "gpt-3.5-turbo"
     system = "you are a helpfull assistant"
     messages = openhistory(filename)
     if messages:
         message= {"role": "user", "content": prompt}
         messages.append(message)
-        response = completion.create(model=model, messages=messages)
+        response = client.chat.completions.create(model=model, messages=messages)
     else:
         messages = [
             {"role": "system", "content": system},
             {"role": "user", "content": prompt}
         ]
-        response = completion.create(model=model, messages=messages)
-    message = response['choices'][0]['message']
+        response = client.chat.completions.create(model=model, messages=messages)
+    message = response.choices[0].message.content
     messages.append(message)
     if filename != 'dontsave':
         savehistory(filename, messages)
@@ -113,44 +106,6 @@ def generate_quiz(summary, num_questions, quiz_type, language="English"):
     response = aicomplete(prompt, filename='dontsave')
     return response
 
-def download_file(url, local_filename):
-    # NOTE the stream=True parameter below
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        with open(local_filename, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192): 
-                # If you have chunk encoded response uncomment if
-                # and set chunk_size parameter to None.
-                #if chunk: 
-                f.write(chunk)
-    return local_filename
-
-def download_youtube_video(url, output_path="."):
-    try:
-        # Create a YouTube object
-        youtube = YouTube(url)
-
-        # Get the highest resolution stream
-        video_stream = youtube.streams.get_highest_resolution()
-
-        # Download the video
-        video_stream.download(output_path)
-
-        print("Download complete!")
-
-    except Exception as e:
-        print(f"Error: {e}")
-
-def get_all_files_in_folder(folder_path):
-    file_list = []
-    
-    # Walk through the folder and its subdirectories
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            file_list.append(os.path.join(root, file))
-            
-    return file_list
-
 def get_quiz(vidUrl, num_questions, quiz_type, language="english"):
     messages = None
     video_id = get_video_id(vidUrl)
@@ -184,51 +139,3 @@ def get_quiz(vidUrl, num_questions, quiz_type, language="english"):
             {"role": "user", "content": prompt}
         ]
     return messages, "filename"
-
-def chatyoutube(vidUrl, user_id, prompt, language="english"):
-    video_id = get_video_id(vidUrl)
-    conversation_id = user_id+video_id
-    filename = "ChatHistory/{}_{}.json".format(user_id, video_id)
-    quiz = openhistory(filename)
-    # summary = summarize(vidUrl, user_id, conversation_id, userinput = 'longv', language=language)
-    video_path = f"Download_Videos/{user_id}_{datetime.datetime.now().timestamp()}"
-    print('downloading video...')
-    download_youtube_video(vidUrl, video_path)
-    # Replace "your_folder_path" with the actual path of the folder you want to list files from
-    files = get_all_files_in_folder(video_path)
-    # download_file(vidUrl, '1_' + video_path)
-    # Load the video 
-    video_path = files[0]
-    video = mp.VideoFileClip(video_path)
-    print('save as audio file')
-    # Extract the audio from the video
-    audio_path = f"{user_id}_{datetime.datetime.now().timestamp()}.wav"
-    if video.duration > 100:
-        start_time = 0
-        end_time = 100
-        cliped_video = video.subclip(start_time, end_time)
-        audio_file = cliped_video.audio
-        audio_file.write_audiofile(audio_path)
-    else:
-        audio_file = video.audio 
-        audio_file.write_audiofile(audio_path)
-    openai.api_key = config.DevelopmentConfig.OPENAI_KEY
-    audio_file= open(audio_path, "rb")
-    transcript = openai.Audio.translate("whisper-1", audio_file)
-    summary = transcript['text']
-    if len(summary) > 8000:
-        summary = summary[: 8000]
-    if summary == "Invalid Video link":
-        return summary
-    if summary == "Invalid YouTube link":
-        return summary
-    if quiz == None:
-        #quiz = get_quiz(vidUrl, user_id, conversation_id, '10', 'true/ false and multiple choice')
-        #quiz  = quiz.replace('\n','<br>' )
-        fprompt = f"This is a summary for a youtube video:\n\n{summary}\n\nprompt: {prompt}\n\nOnly answer the prompt if its related to the summary, respond in {language}"
-    fprompt = f"{quiz}\n\nthis is a quiz for a youtube video, this is it's summary:\n\n{summary}\n\nprompt: {prompt}\n\nOnly answer the prompt if its related to the summary, and you should response wiht answers for each quiz question or problem, respond in {language}"
-    messages = [
-            {"role": "system", "content": "you are a helpfull assistant"},
-            {"role": "user", "content": fprompt}
-        ]
-    return messages, filename
